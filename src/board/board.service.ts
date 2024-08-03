f7040a9e3ad450764fb4455839fa1ffa56fbcbe5
chanseop <dlackstjq23@gmail.com>
2024-08-03 20:37:01 +0900

    board crud

---
+++ src/board/board.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { UpdateBoardDto } from './dto/update-board.dto';
+import { PrismaService } from "src/prisma.service";
+import { board } from '@prisma/client';
+
+@Injectable()
+export class BoardService {
+  constructor(private prisma: PrismaService) {}
+
+  // post
+  async create(createBoardDto: CreateBoardDto): Promise<board> {
+    return this.prisma.board.create({
+      data: {
+        title: createBoardDto.title,
+        contents: createBoardDto.contents,
+        author: createBoardDto.author,
+        category: createBoardDto.category, 
+      },
+    });
+  }
+
+  // get all
+  async findAll(): Promise<board[]> {
+    return this.prisma.board.findMany();
+  }
+
+  findOne(id: number): Promise<board|null> {
+    return this.prisma.board.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: number, updateBoardDto: UpdateBoardDto) : Promise<board> {
+    return this.prisma.board.update({
+      where: { id },
+      data: {
+        title: updateBoardDto.title, 
+        contents: updateBoardDto.contents,
+      },
+    });
+  }
+
+  async remove(id: number): Promise<board> {
+    return this.prisma.board.delete({
+      where: { id },
+    });
+  }
+}
---
+++ src/board/dto/create-board.dto.ts
@@ -0,0 +1,6 @@
+export class CreateBoardDto {
+    title: string;
+    contents: string;
+    author: string;
+    category: number;
+}
