250b228281e263cf7229b737e0e95b1da0ce931e
chanseop <dlackstjq23@gmail.com>
2024-08-04 15:42:55 +0900

    global guard 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthGuard } from "./auth/auth.guard";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
   imports: [BoardModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide:APP_GUARD,
+      useClass:AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
